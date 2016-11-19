28520fda5e42d468223e95643cad4ce505bbfd39
Konstantin Terziev <kd.terziev@gmail.com>
2016-11-19 17:47:32 +0000

    Expression now stores constants.

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -4,15 +4,24 @@ pub mod expressions;
 
 #[cfg(test)]
 mod tests {
-    use math::variables::{AbstractVariable, Variable};
+    use math::variables::{AbstractVariable, AbstractConstant, Variable, Constant};
     use math::relationships::Relationship;
     use math::expressions::Expression;
 
     #[test]
     fn can_create_variables() {
-        let v: Variable = Variable::new("x", 1.0);
+        let v: Variable = AbstractVariable::new("x", 1.0);
         assert_eq!("x", v.name());
-        assert_eq!(1.0, v.coefficient())
+        assert_eq!(1.0, v.coefficient());
+    }
+
+    #[test]
+    fn can_create_constants() {
+        let mut c: Constant = AbstractVariable::new("c", 1.0);
+        c.set_value(365.0);
+        assert_eq!("c", c.name());
+        assert_eq!(1.0, c.coefficient());
+        assert_eq!(365.0, c.value());
     }
 
     #[test]
@@ -39,9 +48,13 @@ mod tests {
 
     #[test]
     fn can_create_expressions() {
-        let expression = Expression::new(vec![Variable::new("Z", 1.0)],
+        let mut c: Constant = AbstractVariable::new("c", 1.0);
+        c.set_value(365.0);
+        let expression = Expression::new(vec![AbstractVariable::new("Z", 1.0)],
                                          Relationship::EQ,
-                                         vec![Variable::new("x", 1.0), Variable::new("y", 2.0)]);
+                                         vec![AbstractVariable::new("x", 1.0),
+                                              AbstractVariable::new("y", 2.0),
+                                              c]);
         assert_eq!("Z", expression.left_hand_side()[0].name());
         assert_eq!(1.0, expression.left_hand_side()[0].coefficient());
         assert_eq!(&Relationship::EQ, expression.relationship());
@@ -49,5 +62,8 @@ mod tests {
         assert_eq!(1.0, expression.right_hand_side()[0].coefficient());
         assert_eq!("y", expression.right_hand_side()[1].name());
         assert_eq!(2.0, expression.right_hand_side()[1].coefficient());
+        assert_eq!("c", expression.right_hand_side()[2].name());
+        assert_eq!(1.0, expression.right_hand_side()[2].coefficient());
+        assert_eq!(365.0, expression.right_hand_side()[2].value())
     }
 }
--- src/math/variables.rs
+++ src/math/variables.rs
@@ -6,9 +6,20 @@ pub trait AbstractVariable {
     fn coefficient(&self) -> f64;
 }
 
+pub trait AbstractConstant: AbstractVariable {
+    fn value(&self) -> f64;
+    fn set_value(&mut self, v: f64);
+}
+
 pub struct Variable {
     name: String,
     coefficient: f64,
+}
+
+pub struct Constant {
+    name: String,
+    coefficient: f64,
+    value: f64,
 }
 
 impl AbstractVariable for Variable {
@@ -25,5 +36,33 @@ impl AbstractVariable for Variable {
 
     fn coefficient(&self) -> f64 {
         self.coefficient
+    }
+}
+
+impl AbstractVariable for Constant {
+    fn new(n: &str, c: f64) -> Constant {
+        Constant {
+            name: n.to_string(),
+            coefficient: c,
+            value: 0.0,
+        }
+    }
+
+    fn name(&self) -> &String {
+        &self.name
+    }
+
+    fn coefficient(&self) -> f64 {
+        self.coefficient
+    }
+}
+
+impl AbstractConstant for Constant {
+    fn value(&self) -> f64 {
+        self.value
+    }
+
+    fn set_value(&mut self, v: f64) {
+        self.value = v;
     }
 }
