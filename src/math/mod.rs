a1f3f625e513b632ccec5c4bc8a5cc1bafbfb69c
Konstantin Terziev <kd.terziev@gmail.com>
2016-11-20 15:46:31 +0000

    Refactored the testing of 'Relationship'.

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -27,23 +27,11 @@ mod tests {
     #[test]
     fn can_create_relationships() {
         let equal: Relationship = Relationship::EQ;
-        let result = match equal {
-            Relationship::EQ => true,
-            _ => false,
-        };
-        assert!(result);
+        assert_eq!(Relationship::EQ, equal);
         let less_than_equal: Relationship = Relationship::LEQ;
-        let result = match less_than_equal {
-            Relationship::LEQ => true,
-            _ => false,
-        };
-        assert!(result);
+        assert_eq!(Relationship::LEQ, less_than_equal);
         let greater_than_equal: Relationship = Relationship::GEQ;
-        let result = match greater_than_equal {
-            Relationship::GEQ => true,
-            _ => false,
-        };
-        assert!(result);
+        assert_eq!(Relationship::GEQ, greater_than_equal);
     }
 
     #[test]
@@ -57,7 +45,7 @@ mod tests {
                                               c]);
         assert_eq!("Z", expression.left_hand_side()[0].name());
         assert_eq!(1.0, expression.left_hand_side()[0].coefficient());
-        assert_eq!(&Relationship::EQ, expression.relationship());
+        assert_eq!(Relationship::EQ, *expression.relationship());
         assert_eq!("x", expression.right_hand_side()[0].name());
         assert_eq!(1.0, expression.right_hand_side()[0].coefficient());
         assert_eq!("y", expression.right_hand_side()[1].name());
